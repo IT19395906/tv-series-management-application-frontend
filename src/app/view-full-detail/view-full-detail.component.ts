c8cdca6b3c2991721055f498aaa2ec5b58df8cb5
IT19395906 <IT19395906@my.sliit.lk>
2025-09-12 00:04:36 +0530

    carousel added

--- src/app/view-full-detail/view-full-detail.component.ts
+++ src/app/view-full-detail/view-full-detail.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
 import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 import { ActivatedRoute, Router } from '@angular/router';
 
@@ -8,10 +8,16 @@ import { ActivatedRoute, Router } from '@angular/router';
   templateUrl: './view-full-detail.component.html',
   styleUrl: './view-full-detail.component.scss'
 })
-export class ViewFullDetailComponent {
+export class ViewFullDetailComponent implements AfterViewInit {
 
   tvSeriesId!: string | null;
   data: any;
+  imageArray: string[] = ["https://static01.nyt.com/images/2025/08/14/12visualUploader-8432-cover/12visualUploader-8432-cover-mobileMasterAt3x-v2.jpg?width=600&quality=75&auto=webp&disable=upscale",
+    "https://static01.nyt.com/images/2025/08/14/12visualUploader-8432-cover/12visualUploader-8432-cover-mobileMasterAt3x-v2.jpg?width=600&quality=75&auto=webp&disable=upscale",
+    "https://static01.nyt.com/images/2025/08/14/12visualUploader-8432-cover/12visualUploader-8432-cover-mobileMasterAt3x-v2.jpg?width=600&quality=75&auto=webp&disable=upscale",
+    "https://static01.nyt.com/images/2025/08/14/12visualUploader-8432-cover/12visualUploader-8432-cover-mobileMasterAt3x-v2.jpg?width=600&quality=75&auto=webp&disable=upscale",
+    "https://static01.nyt.com/images/2025/08/14/12visualUploader-8432-cover/12visualUploader-8432-cover-mobileMasterAt3x-v2.jpg?width=600&quality=75&auto=webp&disable=upscale"
+  ];
   constructor(
     private router: Router,
     private route: ActivatedRoute,
@@ -22,6 +28,31 @@ export class ViewFullDetailComponent {
 
   ngOnInit(): void {
     this.tvSeriesId = this.route.snapshot.paramMap.get('id');
+  }
+
+  ngAfterViewInit(): void {
+    const carousel = document.querySelector('.carousel') as HTMLElement;
+    const left = document.querySelector('#left') as HTMLButtonElement;
+    const right = document.querySelector('#right') as HTMLButtonElement;
+
+    const checkEdge = () => {
+      const scrollLeft = carousel?.scrollLeft;
+      const maxScrollLeft = carousel?.scrollWidth - carousel?.clientWidth;
+      left.disabled = scrollLeft <= 0;
+      right.disabled = scrollLeft >= maxScrollLeft - 1;
+    }
+
+    checkEdge();
+
+    left?.addEventListener('click', () => {
+      carousel?.scrollBy({ left: -500, behavior: 'smooth' });
+    });
+
+    right?.addEventListener('click', () => {
+      carousel?.scrollBy({ left: 500, behavior: 'smooth' });
+    });
+
+    carousel.addEventListener('scroll', checkEdge);
   }
 
   sanitizeUrl(url: string): SafeResourceUrl {
